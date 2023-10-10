1a17cfe301a239a243fe110a8678c7d0afd18e9d
Omar Wael <dev.omarwael@gmail.com>
2023-10-10 03:08:16 +0300

    fixing the main documentation sothat it becomes compliant with betty.

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include<stdio.h>
 
+/**
+ * main - the entry point for the program
+ * Return: a status code (0 if everything went well)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
