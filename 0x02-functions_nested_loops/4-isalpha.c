b9a0d401dccbf47e07f47e566d0800f7e7b28af7
Omar Wael <omarwael19@gmail.com>
2023-10-24 19:51:20 +0300

    writing a fuction that probes an argument against the ASCII codes of alphabetical characters to decide if it corresponds to a lowercase/uppercase alphabetical character

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+/**
+ * _isalpha - probes a given character to decide whether it corresponds to
+ * an ASCII alphabetical character or not
+ * @c: the given character to be compared against ASCII codes for
+ * alphabetical characters
+ * Return: 1 if @c corresponds to an alphabetical character in ASCII;
+ * 0 otherwise
+ */
+int _isalpha(int c)
+{
+if (((c >= 0x61) && (c <= 0x7A)) || ((c >= 41) && (c <= 5A)))
+return (1);
+else
+return (0);
+}
