803f796ab02f2ac0f640d0939377c63d191c56ca
Omar Wael <omarwael19@gmail.com>
2023-10-24 17:50:55 +0300

    Writing a function that prints the English alphabets 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the English language alphabets 10 times
+ * Return: n/a
+ */
+void print_alphabet_x10(void)
+{
+char character = 'a';
+byte iterator = 10;
+
+while (iterator <= 10)
+{
+while (character <= 'z')
+{
+_putchar(character);
+character++;
+}
+iterator++;
+}
+return;
+}
