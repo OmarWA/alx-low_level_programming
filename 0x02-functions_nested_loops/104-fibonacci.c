aaaa2816ece7d3523fbeb831bc0358e99d374d58
Omar Wael <omarwael19@gmail.com>
2023-10-25 00:00:51 +0300

    00

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -4,28 +4,24 @@
  * main - df
  * Return: 0
  */
-int main() {
-    int a = 1, b = 2, count = 2;
-
-    // Print the first two Fibonacci numbers
-    printf("%d, %d, ", a, b);
-
-    // Calculate and print the next 96 Fibonacci numbers
-    while (count < 98) {
-        // Compute the next Fibonacci number
-        int next = a + b;
-
-        // Print the next Fibonacci number with the appropriate separator
-        if (count < 97) {
-            printf("%d, ", next);
-        } else {
-            printf("%d\n", next); // Print a newline after the last number
-        }
-
-        // Update values for the next iteration
-        a = b;
-        b = next;
-        count++;
+int main(void)
+{
+int a = 1, b = 2, count = 2;
+printf("%d, %d, ", a, b);
+while (count < 98)
+{
+int next = a + b;
+if (count < 97)
+{
+printf("%d, ", next);
+}
+else
+{
+printf("%d\n", next);
+}
+a = b;
+b = next;
+count++;
 }
 return (0);
 }
