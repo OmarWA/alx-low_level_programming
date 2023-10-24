bdd3f5d092af7d14bdd1236c6e15f290b0f9e87d
Omar Wael <omarwael19@gmail.com>
2023-10-24 19:39:11 +0300

    Writing a function that probes a given code to decide whether it corresponds to a lowercase ASCII charcter or not

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower - probes a given character to decide whether
+ * it corresponds to a lowercase ascii character or not
+ *
+ * @c: the numerical value to be tested against
+ * ASCII lowercase characters codes
+ *
+ * Return: 1 if c is a lowercase character; 0 otherwise
+ */
+int _islower(int c);
+{
+if ((c >= 0x61) && (c <= 0x7A))
+return (1);
+else
+return (0);
+}
