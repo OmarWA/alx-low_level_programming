53b9e9617aa405b7cbfc23099a38ed282d49f8e1
Omar Wael <omarwael19@gmail.com>
2023-10-24 22:02:48 +0300

    fixing the boundry of the unit-place hours counter

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,10 +7,10 @@
 void jack_bauer(void)
 {
 int hours_units = 0, hours_tens = 0, minutes_units = 0, minutes_tens = 0;
-while (hours_tens < 2)
+while (hours_tens < 3)
 {
 hours_units = 0;
-while (hours_units < 10)
+while (hours_units < ((hours_tens == 2) ? 4 : 10))
 {
 minutes_tens = 0;
 while (minutes_tens < 6)
