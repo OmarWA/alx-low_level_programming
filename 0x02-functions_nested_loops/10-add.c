2e7dd5248c33b8dfcba89b3976bf3bec6454ea6f
Omar Wael <omarwael19@gmail.com>
2023-10-24 22:18:06 +0300

    wirting a function that adds two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,10 @@
+/**
+ * add - adds two numbers and returns their sum
+ * @a: the first number to be added
+ * @b: the second numbers to be added
+ * Return: the sum of @a and @b
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
