56576900c402fb0fbf253b19c2097808bd168d13
Omar Wael <omarwael19@gmail.com>
2023-10-24 21:56:22 +0300

    using _putchar instead of the stdio's printf

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every second of the day
+ * Return: n/a
+ */
+void jack_bauer(void)
+{
+int hours_units = 0, hours_tens = 0, minutes_units = 0, minutes_tens = 0;
+while (hours_tens < 3)
+{
+hours_units = 0;
+while (hours_units < 10)
+{
+minutes_tens = 0;
+while (minutes_tens < 6)
+{
+minutes_units = 0;
+while (minutes_units < 10)
+{
+_putchar('0' + hours_tens);
+_putchar('0' + hours_units);
+_putchar(':');
+_putchar('0' + minutes_tens);
+_putchar('0' + minutes_units);
+_putchar('\n');
+minutes_units++;
+}
+minutes_tens++;
+
+}
+hours_units++;
+}
+hours_tens++;
+}
+return;
+}
