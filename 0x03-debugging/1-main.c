ca45d3dd437bb2def256a69bb1eaa1a025d21d5b
Omar Wael <omarwael19@gmail.com>
2023-11-01 23:48:14 +0200

    debugging a given code to fix an infinite loop.

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point of the program
+ * Return: 0 upon successful completion of the progra
+ */
+int main(void)
+{
+int i;
+
+printf("Infinite loop incoming :(\n");
+
+i = 0;
+
+/*
+*while (i < 10)
+*    {
+*      putchar(i);
+*    }
+*/
+
+printf("Infinite loop avoided \\o/\n");
+
+return (0);
+}
