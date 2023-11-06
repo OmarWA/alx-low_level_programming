e3055996ff31cf6bc39710ab5bbc75be15749361
Omar Wael <omarwael19@gmail.com>
2023-11-06 10:42:50 +0200

    writing a function that multiplies two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,10 @@
+/**
+ * mul - a funciton that evaluates and returns the product of two numbers
+ * @a: a number to be multiplies with @b
+ * @b: another number to be multiplied with @a
+ * Return: the product of @a and @b
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
