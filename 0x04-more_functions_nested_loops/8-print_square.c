39e2c0afd015dd0c3c1e113103a80591f28c65a2
Omar Wael <omarwael19@gmail.com>
2023-11-08 01:35:05 +0200

    sticking to betty linter rules

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,26 +1,29 @@
 #include "main.h"
 
 
+/**
+ * print_square - prints a square of side length = @size
+ * @size: the side length of the square
+ * Return: N/A
+ */
 void print_square(int size)
 {
-  int squareSize = size, squareLength = size, squareWidth = size;
-  
-  if (size <= 0)
-    {
-      _putchar('\n');
-      return;
-    }
-  while (squareLength > 0)
-    {
-      squareWidth = squareSize;
-      while (squareWidth > 0)
-	{
-	  _putchar('#');
-	  squareWidth--;
-	}
-      _putchar('\n');
-      squareLength--;
-    }
-
-  return;
+int squareSize = size, squareLength = size, squareWidth = size;
+if (size <= 0)
+{
+_putchar('\n');
+return;
+}
+while (squareLength > 0)
+{
+squareWidth = squareSize;
+while (squareWidth > 0)
+{
+_putchar('#');
+squareWidth--;
+}
+_putchar('\n');
+squareLength--;
+}
+return;
 }
