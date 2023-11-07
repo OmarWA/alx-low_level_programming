3e54f9f0ce7d2ffaa8d0f63df9341a946983d7f1
Omar Wael <omarwael19@gmail.com>
2023-11-07 23:59:20 +0200

    Writing a function that prints a straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *print_line - Prints @n number of '_' to draw a straight line
+ * @n: the length of the straight line
+ * Return: N/A
+ */
+void print_line(int n)
+{
+while (n > 0)
+{
+_putchar('_');
+n--;
+}
+_putchar('_');
+}
